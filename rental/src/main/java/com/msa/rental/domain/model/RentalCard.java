b7073a6239a8cd427b5c70b5f097877f0a257a2a
hongs429 <honshyeon@gmail.com>
2025-11-02 01:48:57 +0900

    비지니스 개념 테스트 - RentalCard

--- rental/src/main/java/com/msa/rental/domain/model/RentalCard.java
+++ rental/src/main/java/com/msa/rental/domain/model/RentalCard.java
@@ -2,14 +2,22 @@ package com.msa.rental.domain.model;
 
 
 import com.msa.rental.domain.model.vo.IDName;
+import com.msa.rental.domain.model.vo.Item;
 import com.msa.rental.domain.model.vo.LateFee;
 import com.msa.rental.domain.model.vo.RentalCardNo;
 import com.msa.rental.domain.model.vo.RentalStatus;
+import com.msa.rental.domain.model.vo.ReturnItem;
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+/**
+ * 어그리거트
+ */
 
 @Data
 @NoArgsConstructor
@@ -20,21 +28,120 @@ public class RentalCard {
     private RentalStatus rentalStatus;
     private LateFee totalLateFee;
     private List<RentalItem> rentalItems = new ArrayList<>();
-    private List<RentalItem> returnItems = new ArrayList<>();
+    private List<ReturnItem> returnItems = new ArrayList<>();
 
     private void addRentalItem(RentalItem rentalItem) {
-        rentalItems.add(rentalItem);
+        this.rentalItems.add(rentalItem);
     }
 
     private void removeRentalItem(RentalItem rentalItem) {
-        rentalItems.remove(rentalItem);
+        this.rentalItems.remove(rentalItem);
     }
 
-    private void addReturnItem(RentalItem returnItem) {
-        returnItems.add(returnItem);
+    private void addReturnItem(ReturnItem returnItem) {
+        this.returnItems.add(returnItem);
     }
 
-    private void removeReturnItem(RentalItem returnItem) {
-        returnItems.remove(returnItem);
+    private void removeReturnItem(ReturnItem returnItem) {
+        this.returnItems.remove(returnItem);
     }
+
+    // 대여카드 생성
+    public static RentalCard of(IDName creator) {
+        RentalCard rentalCard = new RentalCard();
+
+        rentalCard.setRentalCardNo(RentalCardNo.of());
+        rentalCard.setMember(creator);
+
+        rentalCard.setRentalStatus(RentalStatus.RENT_AVAILABLE);
+        rentalCard.setTotalLateFee(LateFee.createLateFee());
+
+        return rentalCard;
+    }
+
+    // 대여처리
+    public RentalCard rentItem(Item item) {
+        checkRentAvailable();
+        this.addRentalItem(RentalItem.of(item));
+        return this;
+    }
+
+    public RentalCard returnItem(Item item, LocalDate returnDate) {
+        RentalItem rentalItem = this.rentalItems.stream()
+                .filter(i -> i.getItem().equals(item))
+                .findFirst()
+                .orElse(null);
+
+        if (rentalItem == null) {
+            return this;
+        }
+
+        calculateLateFee(rentalItem, returnDate);
+
+        addReturnItem(ReturnItem.of(rentalItem));
+        removeRentalItem(rentalItem);
+        return this;
+    }
+
+    public RentalCard overdueItem(Item item) {
+        RentalItem rentalItem = this.rentalItems.stream()
+                .filter(i -> i.getItem().equals(item))
+                .findFirst()
+                .orElse(null);
+
+        if (rentalItem == null) {
+            return this;
+        }
+
+        rentalItem.setOverdue(true);
+        this.rentalStatus = RentalStatus.RENT_UNAVAILABLE;
+
+        rentalItem.setOverdueDate(LocalDate.now().minusDays(1));
+        return this;
+    }
+
+
+    public long makeAvailableRental(long point) {
+        if (!this.rentalItems.isEmpty()) {
+            throw new IllegalArgumentException("모든 도서가 반납되어야 정지를 해제할 수 있습니다");
+        }
+
+        if (this.getTotalLateFee().getPoint() != point) {
+            throw new IllegalArgumentException("포인트로 연체 해제 불가능");
+        }
+
+        setTotalLateFee(totalLateFee.subtractPoint(point));
+
+        if (this.getTotalLateFee().getPoint() == 0) {
+            this.rentalStatus = RentalStatus.RENT_AVAILABLE;
+        }
+
+        return this.getTotalLateFee().getPoint();
+    }
+
+
+
+
+    private void calculateLateFee(RentalItem rentalItem, LocalDate returnDate) {
+        if (returnDate.isAfter(rentalItem.getOverdueDate())) {
+            long point = Period.between(rentalItem.getOverdueDate(), returnDate).getDays() * 10L;
+
+            this.setTotalLateFee(this.totalLateFee.addPoint(point));
+        }
+    }
+
+    private void checkRentAvailable() {
+        if (this.rentalStatus == RentalStatus.RENT_UNAVAILABLE) {
+            throw new IllegalStateException("Rental card has been unavailable");
+        }
+
+        if (this.rentalItems.size() > 5) {
+            throw new IllegalStateException("Rental card has more than 5 Items");
+        }
+    }
+
+
+
+
+
 }
---
+++ rental/src/test/java/com/msa/rental/ScenarioUnitTest.java
@@ -0,0 +1,88 @@
+package com.msa.rental;
+
+
+import com.msa.rental.domain.model.RentalCard;
+import com.msa.rental.domain.model.RentalItem;
+import com.msa.rental.domain.model.vo.IDName;
+import com.msa.rental.domain.model.vo.Item;
+import com.msa.rental.domain.model.vo.ReturnItem;
+import java.time.LocalDate;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
+public class ScenarioUnitTest {
+
+
+    @Test
+    void test() {
+
+        IDName hongs = IDName.of("1", "홍승현");
+
+        RentalCard rentalCard = RentalCard.of(hongs);
+
+        showCardStatus(rentalCard);
+
+        Item item1 = Item.of(1, "도서1");
+        Item item2 = Item.of(2, "도서2");
+
+        rendAndShowStatus(rentalCard, item1);
+        rendAndShowStatus(rentalCard, item2);
+        returnAndShowStatus(rentalCard, item1);
+        overdueAndShowStatus(rentalCard, item2);
+        returnAndShowStatus(rentalCard, item2);
+        makeAvailableAndShowStatus(rentalCard);
+
+    }
+
+    private void makeAvailableAndShowStatus(RentalCard rentalCard) {
+        System.out.println("현재 남은 연체료는 " + rentalCard.getTotalLateFee().getPoint());
+        rentalCard.makeAvailableRental(rentalCard.getTotalLateFee().getPoint());
+        System.out.println("정지해제 처리");
+        showCardStatus(rentalCard);
+    }
+
+    private void overdueAndShowStatus(RentalCard rentalCard, Item item) {
+        rentalCard.overdueItem(item);
+        System.out.println(" 도서명: " + item.getTitle() + " 강제 연체");
+        showCardStatus(rentalCard);
+    }
+
+    private void returnAndShowStatus(RentalCard rentalCard, Item item) {
+        rentalCard.returnItem(item, LocalDate.now());
+        System.out.println(" 도서명: " + item.getTitle() + " 반납됨");
+        showCardStatus(rentalCard);
+    }
+
+    private void rendAndShowStatus(RentalCard rentalCard, Item item) {
+        rentalCard.rentItem(item);
+        System.out.println(" 도서명: " + item.getTitle() + " 대여됨");
+        showCardStatus(rentalCard);
+    }
+
+    private void showCardStatus(RentalCard card) {
+        System.out.println("⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐");
+        System.out.println(" " + card.getMember().getName() + " 도서카드 ");
+        System.out.println(" 대여도서 연체상태 : " + card.getRentalItems().stream().map(RentalItem::isOverdue).toList());
+        System.out.println(" 총 연체료 : " + card.getTotalLateFee().getPoint());
+        System.out.println(" 대여가능 여부 : " + card.getRentalStatus().toString());
+        System.out.println(" 대여 목록 ");
+        System.out.println(
+                card.getRentalItems().stream()
+                        .map(RentalItem::getItem)
+                        .map(Item::getTitle)
+                        .toList()
+        );
+        System.out.println(" 반납 목록 ");
+        System.out.println(
+                card.getReturnItems().stream()
+                        .map(ReturnItem::getRentalItem)
+                        .map(RentalItem::getItem)
+                        .map(Item::getTitle)
+                        .toList()
+        );
+        System.out.println("⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐");
+    }
+}
